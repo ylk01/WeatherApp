75b1cf2555e2a69a89fc66133732a41b2079ff4f
ylk01 <fvd130@aubg.edu>
2016-10-15 17:52:53 +0300

    First Commit

---
+++ return.php
@@ -0,0 +1,22 @@
+<?PHP
+
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+/*$img = 'http://localhost/wpf/13.png';
+header('Content-Type: image/png');
+readfile($img);*/
+
+$city = $_POST['cityname'];
+
+$json = file_get_contents("http://api.openweathermap.org/data/2.5/weather?q='.$city.'&APPID=7f759089493b563470025ff8e11b88c9");
+
+$data = json_decode($json);
+
+echo "<center><h1> It is " . $data->weather[0]->description . " in " . $data->name . " now.</h1></br>";
+echo "<h1> Temperature is " . round(($data->main->temp-273.15)) . " C</h1></br>";
+echo "<h1> Humidity is " . $data->main->humidity . " %</h1></br>"; 
+echo "<h1> Pressure is " . round($data->main->pressure) . " Mbar</h1></br>"; 
+echo "<h1> Wind is " . round($data->wind->speed*3.6) . " Km/h</h1></br>";
+
+
+?>
